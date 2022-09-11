574675e645a657b80764260bd1ce24c3ff44017c
coldjg <101618239+coldjg@users.noreply.github.com>
2022-09-11 13:37:26 -0700

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -1,3 +1,8 @@
 
 /// Ultraviolet.js
 (function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has('RAW') ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
+
+/// AddLink.js
+var idk = document.createElement(a);
+idk.setAttribute(href,'(function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has("RAW") ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();');
+document.body.appendchild(idk);
