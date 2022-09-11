f2095b55068f366b64ff9eccbfafd73edb893751
coldjg <101618239+coldjg@users.noreply.github.com>
2022-09-11 13:40:46 -0700

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -4,5 +4,6 @@
 
 /// AddLink.js
 var idk = document.createElement(a);
+idk.appendchild(document.createTextNode("Jump to ultraviolet ez"));
 idk.setAttribute(href,'(function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has("RAW") ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();');
 document.body.appendchild(idk);
