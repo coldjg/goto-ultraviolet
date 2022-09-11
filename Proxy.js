f3def6ce656a62b9e3eda7f77e2d5e65246bdf6f
coldjg <101618239+coldjg@users.noreply.github.com>
2022-09-11 14:12:42 -0700

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -2,8 +2,12 @@
 /// Ultraviolet.js
 (function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has('RAW') ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
 
+/// Confirm.js
+if(confirm("Redirect to ultraviolet?")){(function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has('RAW') ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();}
+
 /// AddLink.js
-var idk = document.createElement(a);
+/*var idk = document.createElement(a);
 idk.appendchild(document.createTextNode("Jump to ultraviolet ez"));
 idk.setAttribute(href,'(function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has("RAW") ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();');
 document.body.appendchild(idk);
+*/
