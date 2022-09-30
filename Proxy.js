fd7c816000af2a81773f508d00a6c90b5c0833bc
coldjg <101618239+coldjg@users.noreply.github.com>
2022-09-29 19:55:06 -0700

    Add Eurda
    inspect element

--- Proxy.js
+++ Proxy.js
@@ -5,6 +5,9 @@
 /// Confirm.js
 if(confirm("Redirect to ultraviolet?")){(function(){javascript:winloc = window.location; searchparam = new URLSearchParams(winloc.search); url = searchparam.has('RAW') ? searchparam.get("RAW") : winloc.href.toString(); "http" != url.substring(0, 4) && (url = "https://" + url); winloc.href = "https://huapptesthingy.herokuapp.com/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();}
 
+/// Eurda.js
+(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+
 /// AddLink.js
 /*var idk = document.createElement(a);
 idk.appendchild(document.createTextNode("Jump to ultraviolet ez"));
